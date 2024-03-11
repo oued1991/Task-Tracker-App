1163ca89614b7851041cc68c1404e17e7307b385
oued1991 <o_abass40@yahoo.com>
2024-03-10 22:49:16 -0400

    Implement task rendering

--- script.js
+++ script.js
@@ -41,13 +41,25 @@ function handleSubmission(event) {
         deadline: taskDeadline
     });
 
-    
+
     render();
 }
 // Function to render tasks in the table
 function render() {
     // TODO: Use array methods to create a new table row of data for each item in the arr
+
+
+    taskTable.innerHTML = tasks.map(task => `
+    <tr>
+        <td>${task.name}</td>
+        <td>${task.description}</td>
+        <td>${task.deadline}</td>
+        <td><button onclick="markTaskComplete(this)">Complete</button></td>
+        <td><button onclick="removeTask(this)">Remove</button></td>
+    </tr>
+`).join('');
 }
+
 // Function to initialize the table
 function init() {
     taskTable.innerHTML = ''; // Clear the table
